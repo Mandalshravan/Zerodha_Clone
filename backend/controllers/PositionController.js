e28f41b5b124099b61493f4ee044c55e2ff58fd3
Mandalshravan <mandalshravan2004@gmail.com>
2025-06-27 20:52:47 +0530

    Initial commit

---
+++ backend/controllers/PositionController.js
@@ -0,0 +1,11 @@
+const Positions = require("../model/PositionsModel");
+
+module.exports.allPositions = async (req, res) => {
+  try {
+    let allPositions = await Positions.find({});
+    res.json(allPositions);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
---
+++ backend/index.js
@@ -0,0 +1,45 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const PORT = process.env.PORT || 3002;
+const uri = process.env.MONGO_URL;
+
+const authRoute = require("./routes/AuthRoute");
+const ordersRoute = require("./routes/OrderRoute");
+const holdingsRoute = require("./routes/HoldingRoute");
+const positionsRoute = require("./routes/PositionRoute");
+
+const app = express();
+app.use(
+  cors({
+    origin: ["https://zerodha-frontend-s5ov.onrender.com/", "https://zerodha-dashboard-33xb.onrender.com/"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(express.json());
+
+app.use("/auth", authRoute);
+app.use("/order", ordersRoute);
+app.use("/holding", holdingsRoute);
+app.use("/position", positionsRoute);
+
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
